a058c4441d53a5751802b2f1eb513480c5905913
parada15 <luisparada0085@gmail.com>
2025-10-02 12:06:24 -0600

    media table

---
+++ ejercicio1/public/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+    <thead>
+<tr>
+<th>Mes</th>
+<th>Cuota</th>
+<th>Intereses</th>
+<th>Capital</th>
+<th>Saldo</th>
+
+</tr>
+
+    </thead>
+    </table>
+</body>
+</html>
